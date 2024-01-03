40ab7f7235c7506885197b01c53015c89852b581
Thejaswin Kumaran <TXK210042@utdallas.edu>
2024-01-02 22:58:43 -0600

    FE Development

--- svelte-front-end/src/App.svelte
+++ svelte-front-end/src/App.svelte
@@ -1,30 +1,36 @@
 <script>
 	export let name;
+
+	let featherSrc = '/Feather.png';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
-	main {
+	main
+	{
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h1 {
+	h1
+	{
 		color: #ff3e00;
-		text-transform: uppercase;
+		/* text-transform: uppercase; */
 		font-size: 4em;
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
+	@media (min-width: 640px)
+	{
 		main {
 			max-width: none;
 		}
 	}
 </style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
