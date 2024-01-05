72b2bec66d77366447074541f74bafe815eb62d1
Thejaswin Kumaran <TXK210042@utdallas.edu>
2024-01-04 23:12:50 -0600

    FE Development -T

--- svelte-front-end/src/App.svelte
+++ svelte-front-end/src/App.svelte
@@ -1,11 +1,21 @@
 <script>
+	import SI from './socialInfo.svelte';
+
+
 	let featherSrc = '/Feather.png';
 </script>
 
 <style>
+	:global(body)
+	{
+		background-color: #f9f7e9;
+	}
 
 </style>
 
-<img src = {featherSrc} width = "135em" alt = "NotePal Logo">
+<img src = {featherSrc} width = "120em" alt = "NotePal Logo">
 
-<input type = "text" value = "ex. <q>CS 3377<q> or <q>UNIX<q>" disabled>
+<!-- <input type = "text" value = "ex. <q>CS 3377<q> or <q>UNIX<q>" disabled> -->
+
+<SI>
+</SI>
