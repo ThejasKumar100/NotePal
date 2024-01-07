2c8e348f36beba99afa4acb79e2b22d23b503804
Thejaswin Kumaran <TXK210042@utdallas.edu>
2024-01-07 13:10:26 -0600

    FE Dev, Kit Setup

--- svelteKit-front-end/npkit/src/routes/+page.svelte
+++ svelteKit-front-end/npkit/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Origin Page -->
+
+<script>
+	// import SI from './socialInfo.svelte';
+	// import Home from './home.svelte';
+	// import SearchCom from './search.svelte';
+</script>
+
+<style>
+	:global(body)
+	{
+		background-color: #f9f7ea;
+	}
+
+	
+</style>
+
+
+
+
+<!-- <SI></SI> -->
+<!-- <Home></Home> -->
